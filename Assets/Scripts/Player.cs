15e251cf7042059dd8d8d21997315c0e9fd360eb
illiden228 <den.khabibullin@gmail.com>
2020-02-10 02:36:30 +0300

    Add Script "Player". correct animations, configure particle system Shoot

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Health;
+    public ParticleSystem Shootgun;
+
+    private float _currentHealth;
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = Health;
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shootgun.Play();
+            _animator.Play("Shoot");
+        }
+    }
+
+    public void Attack(int damage)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, Health);
+    }
+}
