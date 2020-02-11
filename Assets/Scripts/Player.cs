c8d9dc674a29ca45fac04346c96d9cabb64cacb3
illiden228 <den.khabibullin@gmail.com>
2020-02-12 02:13:13 +0300

    Create Spawner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,6 +29,5 @@ public class Player : MonoBehaviour
     public void Attack(int damage)
     {
         _currentHealth = Mathf.Clamp(_currentHealth - damage, 0, Health);
-        Debug.Log(_currentHealth);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<Wave> Waves;
+    public Player Player;
+
+    private Wave _currentWave;
+    private int _waveCount;
+    private float _lastSpawnTime;
+    private int _spawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetWave(_waveCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentWave == null)
+        {
+            return;
+        }
+
+        _lastSpawnTime += Time.deltaTime;
+        if(_lastSpawnTime >= _currentWave.EnemyBetweenDelay)
+        {
+            var enemy = Instantiate(_currentWave.EnemyPrefab, transform.position, transform.rotation, transform);
+            enemy.GetComponent<Enemy>().SetTarget(Player);
+            _spawned++;
+            _lastSpawnTime = 0;
+        }
+        
+        if(_currentWave.EnemyCount <= _spawned)
+        {
+            if(Waves.Count > _waveCount)
+            {
+                SetWave(++_waveCount);
+                _spawned = 0;
+            }
+            else
+            {
+                _currentWave = null;
+            }
+        }
+        Debug.Log("CurrentWave: " + _currentWave + ", WaveCount: " + _waveCount + ", Spawned: " + _spawned + ", EnemyCount: " + _currentWave.EnemyCount);
+        Debug.Log("-----------------------");
+    }
+
+    public void SetWave(int index)
+    {
+        _currentWave = Waves[index];
+    }
+}
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject EnemyPrefab;
+    public float EnemyBetweenDelay;
+    public int EnemyCount;
+}
