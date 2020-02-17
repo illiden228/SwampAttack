542302241dbcb78235f5223accc495b66a3056c7
illiden228 <den.khabibullin@gmail.com>
2020-02-17 08:17:41 +0300

    Shop

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Weapon
+{
+    public string Name;
+    public int Price;
+    public Sprite Icon;
+    public ParticleSystem ShootEffect;
+    public bool IsBuy = false;
+}
